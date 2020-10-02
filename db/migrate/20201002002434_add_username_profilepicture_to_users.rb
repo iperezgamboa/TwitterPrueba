d4188ad86bf6cad1e3a09fffa76a134df93dc594
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2020-10-01 21:37:43 -0300

    controllers,routes,deviseuser ,devisecontrollersusers,migracion

---
+++ db/migrate/20201002002434_add_username_profilepicture_to_users.rb
@@ -0,0 +1,6 @@
+class AddUsernameProfilepictureToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :profile_picture, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_215316) do
+ActiveRecord::Schema.define(version: 2020_10_02_002434) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_10_01_215316) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "profile_picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

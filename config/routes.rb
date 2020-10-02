14b5bdf479a79ee5e692dca3d746f2fc2e23507f
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2020-10-01 22:30:49 -0300

    dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :tweets
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-   root "homes#index"
-   resources :homes
+   root "tweets#index"
+   
 end

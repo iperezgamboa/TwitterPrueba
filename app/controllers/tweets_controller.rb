55e7812fd6951e4035f2215b4ccf89a52af346c6
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-02-13 16:05:57 -0300

    notes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  #this keep those who are not signed in-out of our app for the most part.
   before_action :authenticate_user!, except: [:index]
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 

ea6d8aaefbfb96dba722b88ca3abe65858c65999
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-02-13 22:46:04 -0300

    tweet content validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   
   belongs_to :user
+  validates :content, presence: true, length: { maximum: 140 }
   has_many :likes, dependent: :destroy
   has_many :tweets
 end

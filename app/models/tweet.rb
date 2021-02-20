397d064726400fce8e276a0af000a4cd359ab363
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-02-20 01:08:51 -0300

    changes

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,5 +3,9 @@ class Tweet < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
   has_many :likes, dependent: :destroy
+  has_many :linking_users, :through => :likes, :source => :user
+  paginates_per 50
   has_many :tweets
+  belongs_to :tweets, optional: true
+   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tweets
-  has_many :likes, dependent: :destroy
+  has_many :tweets, dependent: :destroy #Los tweets se eliminan si la cuenta es eliminada
+  has_many :likes, dependent: :destroy #Los likes se eliminan si la cuenta es eliminada
   paginates_per 50
   
 

60aa2a9a052ce7ff89494c5cdf19e6124294372a
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-02-26 22:16:50 -0300

    friend model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_004922) do
+ActiveRecord::Schema.define(version: 2021_02_27_011610) do
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "friend_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "tweet_id"

d6299149d0f8dfadbf100008bcff0e69372115a5
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-03-14 00:57:21 -0300

    sign_in view fixed

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,21 +1,19 @@
 class ApiController < ActionController::API
-  #http_basic_authenticate_with :name => "user", :password => "password"
-  
-  before_action :authenticate_user!
-
-  def create_api_tweet 
     
-        @user = current_user
-          if @user.present?
-              @tweeet = Tweet.new(content: request.headers["X-CONTENT"], user: @user)
-              if @tweeet.save
-                  render json: @tweeet
-              else
-                  render json: "Tweet couldn't be saved"
-              end
+before_action :authenticate_user!
+
+def create_api_tweet 
+  
+    @user = current_user
+      if @user.present?
+          @tweeet = Tweet.new(content: request.headers["X-CONTENT"], user: @user)
+          if @tweeet.save
+              render json: @tweeet
+          else
+              render json: "Tweet couldn't be saved"
+          end
           else
               render json: "User not found"
-          end
-        end
+      end
+    end
 end
-

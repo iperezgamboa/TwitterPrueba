79e51ba99182df01acfeb0abf192024d8bbda88b
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-03-13 23:40:42 -0300

    historia3

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,11 @@
 class ApiController < ActionController::API
+  #http_basic_authenticate_with :name => "user", :password => "password"
+  
+  before_action :authenticate_user!
+
   def create_api_tweet 
     
-        @user = User.find_by(email: request.headers["X-EMAIL"])
+        @user = current_user
           if @user.present?
               @tweeet = Tweet.new(content: request.headers["X-CONTENT"], user: @user)
               if @tweeet.save
@@ -12,5 +16,6 @@ class ApiController < ActionController::API
           else
               render json: "User not found"
           end
-  end
+        end
 end
+
